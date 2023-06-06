aaae6ded75f60ecc791451d290d3f2105dcaf0ff
joestack <joern@joern-online.de>
2023-06-06 09:11:49 +0200

    clean 10

--- main.tf
+++ main.tf
@@ -1,15 +1,24 @@
-// provider and provider related or globally used data sources
-provider "aws" {
-  region = var.aws_region
+# // provider and provider related or globally used data sources
+# provider "aws" {
+#   region = var.aws_region
+# }
+
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
 }
 
-
 locals {
-  image_id = data.aws_ami.boundary.id
+  #image_id = data.aws_ami.boundary.id
 
-  private_subnets = coalescelist(var.private_subnets, module.vpc.private_subnets)
+  #private_subnets = coalescelist(var.private_subnets, module.vpc.private_subnets)
 
-  public_subnets = coalescelist(var.public_subnets, module.vpc.public_subnets)
+  #public_subnets = coalescelist(var.public_subnets, module.vpc.public_subnets)
 
   tags = merge(
     var.tags,
@@ -22,18 +31,18 @@ locals {
 }
 
 
-resource "aws_key_pair" "aws-hashistack-key" {
-  count      = var.key_name != "aws-hashistack-key" ? 0 : 1
-  key_name   = "aws-hashistack-key"
-  public_key = var.aws_hashistack_key
-}
+# resource "aws_key_pair" "aws-hashistack-key" {
+#   count      = var.key_name != "aws-hashistack-key" ? 0 : 1
+#   key_name   = "aws-hashistack-key"
+#   public_key = var.aws_hashistack_key
+# }
 
 data "aws_availability_zones" "available" {}
 
-data "aws_route53_zone" "selected" {
-  name         = "${var.dns_domain}."
-  private_zone = false
-}
+# data "aws_route53_zone" "selected" {
+#   name         = "${var.dns_domain}."
+#   private_zone = false
+# }
 
 data "aws_ami" "boundary" {
   most_recent = true
@@ -55,7 +64,7 @@ module "vpc" {
   version = "~> 3.7"
 
   azs                = data.aws_availability_zones.available.names
-  cidr               = var.network_address_space
+  cidr               = var.cidr_block
   create_vpc         = var.vpc_id != "" ? false : true
   enable_nat_gateway = true
   enable_dns_hostnames = true
@@ -76,67 +85,67 @@ module "vpc" {
   tags = local.tags
 }
 
-### ALB
+# ### ALB
 
-module "alb" {
-  source  = "terraform-aws-modules/alb/aws"
-  version = "~> 6.5"
+# module "alb" {
+#   source  = "terraform-aws-modules/alb/aws"
+#   version = "~> 6.5"
 
-  http_tcp_listeners = [
-    {
-      port     = 80
-      protocol = "HTTP"
-    }
-  ]
+#   http_tcp_listeners = [
+#     {
+#       port     = 80
+#       protocol = "HTTP"
+#     }
+#   ]
 
-  load_balancer_type = "application"
-  name               = "boundary"
-  security_groups    = [aws_security_group.alb.id]
-  subnets            = local.public_subnets
-  tags               = local.tags
+#   load_balancer_type = "application"
+#   name               = "boundary"
+#   security_groups    = [aws_security_group.alb.id]
+#   subnets            = local.public_subnets
+#   tags               = local.tags
   
-  target_groups = [
-    {
-      name             = "boundary"
-      backend_protocol = "HTTP"
-      backend_port     = 9200
-    }
-  ]
+#   target_groups = [
+#     {
+#       name             = "boundary"
+#       backend_protocol = "HTTP"
+#       backend_port     = 9200
+#     }
+#   ]
 
-  vpc_id = local.vpc_id
-}
+#   vpc_id = local.vpc_id
+# }
 
 
-resource "aws_security_group" "bastion" {
-  count = var.key_name != "" ? 1 : 0
+# resource "aws_security_group" "bastion" {
+#   count = var.key_name != "" ? 1 : 0
 
-  egress {
-    cidr_blocks = ["0.0.0.0/0"]
-    from_port   = 0
-    protocol    = "-1"
-    to_port     = 0
-  }
+#   egress {
+#     cidr_blocks = ["0.0.0.0/0"]
+#     from_port   = 0
+#     protocol    = "-1"
+#     to_port     = 0
+#   }
 
-  ingress {
-    cidr_blocks = ["0.0.0.0/0"]
-    from_port   = 22
-    protocol    = "TCP"
-    to_port     = 22
-  }
+#   ingress {
+#     cidr_blocks = ["0.0.0.0/0"]
+#     from_port   = 22
+#     protocol    = "TCP"
+#     to_port     = 22
+#   }
 
-  name   = "Boundary Bastion"
-  tags   = var.tags
-  vpc_id = local.vpc_id
-}
+#   name   = "Boundary Bastion"
+#   tags   = var.tags
+#   vpc_id = local.vpc_id
+# }
 
-resource "aws_instance" "bastion" {
-  count = var.key_name != "" ? 1 : 0
+# resource "aws_instance" "bastion" {
+#   count = var.key_name != "" ? 1 : 0
 
-  ami                         = var.image_id
-  associate_public_ip_address = true
-  instance_type               = "t3.micro"
-  key_name                    = var.key_name
-  subnet_id                   = local.public_subnets[0]
-  tags                        = merge(var.tags, { Name = "Boundary Bastion" })
-  vpc_security_group_ids      = [one(aws_security_group.bastion[*].id)]
-}
+#   ami                         = var.image_id
+#   associate_public_ip_address = true
+#   instance_type               = "t3.micro"
+#   key_name                    = var.key_name
+#   subnet_id                   = local.public_subnets[0]
+#   tags                        = merge(var.tags, { Name = "Boundary Bastion" })
+#   vpc_security_group_ids      = [one(aws_security_group.bastion[*].id)]
+# }
--- variables.tf
+++ variables.tf
@@ -10,107 +10,113 @@ variable "name" {
 
 variable "vpc_id" {
   description = "The ID of the VPC"
-  default = ""
+  default     = ""
   type        = string
 }
 
-variable "private_subnets" {
-  description = "List of private subnet ids"
-  default = []
-  type        = list(string)
-}
-
-variable "public_subnets" {
-  description = "List of public subnet ids"
-  default = []
-  type        = list(string)
-}
-
-variable "tags" {
-  default = {}
-
-  description = <<EOF
-One or more tags. You can tag your Auto Scaling group and propagate the tags to
-the Amazon EC2 instances it launches.
-EOF
-
-  type = map(string)
-}
-
-variable "server_count" {
-  description = "Amount of cluster instances (odd number 1,3, max 5)"
-  default     = "3"
-}
-
-variable "instance_type" {
-  description = "Type of EC2 cluster instance"
-  default     = "t2.small"
-}
-
-# variable "server_name" {
-#   default = "hc-stack-srv"
-# }
-
-variable "root_block_device_size" {
-  default = "80"
-}
-
-variable "auto_join_value" {
-  description = "Server rejoin tag_value to identify cluster instances"
-  default     = "joestack_hashistack_autojoin"
-}
-
-variable "dns_domain" {
-  description = "The Route53 Zone to assign DNS records to"
-}
-
-variable "key_name" {
-  description = "SSH key name to be used to access any instances. Use the one that already exist in your AWS region or keep the default and assign the pub key to aws_hashistack_key variable"
-  default     = "aws-hashistack-key"
-}
-
-variable "aws_hashistack_key" {
-  description = "The public part of the SSH key to access any instance"
-  default     = "NULL"
-}
-
-variable "whitelist_ip" {
-  description = "The allowed ingress IP CIDR assigned to the ASGs"
-  default     = "0.0.0.0/0"
-}
-
-variable "network_address_space" {
+variable "cidr_block" {
   description = "The default CIDR to use"
   default     = "10.0.0.0/16"
 }
 
-// GLOBAL CERT SETTINGS
-
-variable "create_root_ca" {
-  description = "Create a self-signed root ca based on hashicorp/terraform-provider-tls"
-  default     = "true"
-}
-
-variable "common_name" {
-  description = "Cert common name"
-  default     = "hashistack"
-}
-
-variable "organization" {
-  description = "Cert Organaization"
+variable "key_name" {
+  description = "SSH key name to be used to access any instances. Use the one that already exist in your AWS region or keep the default and assign the pub key to aws_hashistack_key variable"
   default     = "joestack"
 }
 
-//BOUNDARY SETTINGS
 
-variable "boundary_enabled" {
-  default = "false"
-}
 
-variable "boundary_version" {
-  default = "0.12.2-1"
-}
+# variable "private_subnets" {
+#   description = "List of private subnet ids"
+#   default = []
+#   type        = list(string)
+# }
 
-variable "boundary_lic" {
-  default = "NULL"
-}
+# variable "public_subnets" {
+#   description = "List of public subnet ids"
+#   default = []
+#   type        = list(string)
+# }
+
+# variable "tags" {
+#   default = {}
+
+#   description = <<EOF
+# One or more tags. You can tag your Auto Scaling group and propagate the tags to
+# the Amazon EC2 instances it launches.
+# EOF
+
+#   type = map(string)
+# }
+
+# variable "server_count" {
+#   description = "Amount of cluster instances (odd number 1,3, max 5)"
+#   default     = "3"
+# }
+
+# variable "instance_type" {
+#   description = "Type of EC2 cluster instance"
+#   default     = "t2.small"
+# }
+
+# # variable "server_name" {
+# #   default = "hc-stack-srv"
+# # }
+
+# variable "root_block_device_size" {
+#   default = "80"
+# }
+
+# variable "auto_join_value" {
+#   description = "Server rejoin tag_value to identify cluster instances"
+#   default     = "joestack_hashistack_autojoin"
+# }
+
+# variable "dns_domain" {
+#   description = "The Route53 Zone to assign DNS records to"
+# }
+
+
+
+# variable "aws_hashistack_key" {
+#   description = "The public part of the SSH key to access any instance"
+#   default     = "NULL"
+# }
+
+# variable "whitelist_ip" {
+#   description = "The allowed ingress IP CIDR assigned to the ASGs"
+#   default     = "0.0.0.0/0"
+# }
+
+
+
+# // GLOBAL CERT SETTINGS
+
+# variable "create_root_ca" {
+#   description = "Create a self-signed root ca based on hashicorp/terraform-provider-tls"
+#   default     = "true"
+# }
+
+# variable "common_name" {
+#   description = "Cert common name"
+#   default     = "hashistack"
+# }
+
+# variable "organization" {
+#   description = "Cert Organaization"
+#   default     = "joestack"
+# }
+
+# //BOUNDARY SETTINGS
+
+# variable "boundary_enabled" {
+#   default = "false"
+# }
+
+# variable "boundary_version" {
+#   default = "0.12.2-1"
+# }
+
+# variable "boundary_lic" {
+#   default = "NULL"
+# }
